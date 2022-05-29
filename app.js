ab860cb185c104e385cc4fb4e3536710226f8fc5
trdmitr <trdmitr@gmail.com>
2022-05-29 11:45:36 +0400

    splin

--- app.js
+++ app.js
@@ -4,53 +4,71 @@ document.addEventListener('DOMContentLoaded', () => {
     */
     const src = [
       [
-        "Виталий Подземный", "Рождённый в СССР", "https://drive.google.com/uc?export=download&id=1mOxCkT1WHdA4c--jmmNvrZE9cDFs4cI9",
-         "https://sun9-31.userapi.com/s/v1/if1/rB8uVMVbb8at_lsfKUImIRM2NgEHAtiFW7l6hloIDnaU-rYNPaZOVItf9vpRqnH9ggvEWW_H.jpg?size=1350x900&quality=96&type=album"
+        "Виталий Подземный", "Черная Волга", "https://drive.google.com/uc?export=download&id=11lr_5AS2CW-Sc5GNuV1dyXlmgBNyGeKG",
+        "https://sun9-west.userapi.com/sun9-54/s/v1/if2/Kas3ZayJT1cUz0XqcTgruytNGALR64AAD2559vtgUao9BdO8Nqr20sbvjoLgv3hRswfnR_GtbpzFkZlkfpxdCGbt.jpg?size=2560x1824&quality=96&type=album"
+      ],
+
+      [
+        "Виталий Подземный", "Оркестр", "https://drive.google.com/uc?export=download&id=18qiNsOlitUBshi44eTwBZ_bDFsrmZ633",
+        "https://sun9-west.userapi.com/sun9-54/s/v1/if2/Kas3ZayJT1cUz0XqcTgruytNGALR64AAD2559vtgUao9BdO8Nqr20sbvjoLgv3hRswfnR_GtbpzFkZlkfpxdCGbt.jpg?size=2560x1824&quality=96&type=album"
       ],
       [
-        "Виталий Подземный", "Кладбище", "https://drive.google.com/uc?export=download&id=1_T0vr2oS2mws2tTzsJ9zuTEMt6t3ze-Z",
-        "https://sun9-25.userapi.com/s/v1/if1/uq7utp5-0_n24xRpNX1bdqvYCO1_KsvlKfK0MT7rV8a2mvsT_teu880guhBhbUj6CdrFgw.jpg?size=537x807&quality=96&type=album"
+        "Artyom Smallkid", "Рай в шалаше", 
+        "https://drive.google.com/uc?export=download&id=1iQFYzTT8z19bNpdivUxSGl6fE_vZoLYA",
+        "https://sun9-east.userapi.com/sun9-20/s/v1/if1/RV3MYsymr16997zW5iqpXpkkJxQJaY0CRLb4djtWShX7bCRdQu96JMvpvbhNnAoy472sUg.jpg?size=2560x1696&quality=96&type=album"
+      ],
+      
+
+      [
+        "Серж Левинс ", "Скоро будет солнечно", 
+        "https://drive.google.com/uc?export=download&id=1NaU5S7xgIwvOMV2e2knV6Nc3-dzq6hTF",
+        "https://sun9-north.userapi.com/sun9-77/s/v1/if2/30qzQGGEyz7qm4bc63I5RBiedjNM7JiAn8O2HFVzZAntsnTqQRTMTSQtlr5ZvGmccV-6VHiZ_7xFg1eI0xv6yXFZ.jpg?size=1727x927&quality=95&type=album"
+      ],
+      
+      [
+        "Дворовый стиль", "Письмо", "https://drive.google.com/uc?export=download&id=1MdIn8mkCZBs_cgUaipaOBMzqpehd7mdR",
+         "https://sun9-west.userapi.com/sun9-56/s/v1/if1/3aNTcY7iHbySv6CA4ezw5A2aYHLv89gnEC-DSYEqLK1tBMIfr5ohiAy9E-Fdxda3H25lwfLT.jpg?size=988x1080&quality=96&type=album"
       ],
       [
-        "Виталий Подземный", "Московская барыня", "https://drive.google.com/uc?export=download&id=1lDo4nnz6dcpSLsif6jaNCa2O52hDmkRo",
-        "https://sun9-77.userapi.com/s/v1/if1/YswOKXKRdKUQH57fpqMz8R612hvPDczY6BVE3-C_aarKsV7hBQkyVrjHHzFZs6lh8ULC-w.jpg?size=537x807&quality=96&type=album"
+        "Константин Рубан", "Небо в алмазах", "https://drive.google.com/uc?export=download&id=1oerxx4CIHtaBoQdRAxF3CvixNSa5tlWw",
+         "https://sun9-west.userapi.com/sun9-3/s/v1/if2/XxipmDPI3_hS6pK1jmoX9lQoQHFnnWvCVy1qsZxwDhgPGeFQCQpflVX60mXyZPFQpW73EiRdRls98x9VVKtChWIM.jpg?size=1620x2160&quality=95&type=album"
       ],
       [
-        "Artyom Smallkid", "Капитан Колесников", 
-        "https://drive.google.com/uc?export=download&id=1iqRaWBY-XAU5UwUHZidsMNQowM1KrlbH",
-        "https://sun9-10.userapi.com/s/v1/if1/Xe8-4wyQRRl-fmiefRzI9uCcQG4DGNqx16mYfyihyz8T8JUkwbKI9wMeWBQ03LmYTdbmvw.jpg?size=1080x1920&quality=96&type=album"
+        "Дворовый стиль", "Бог устал нас любить", "https://drive.google.com/uc?export=download&id=1DeDrRZpmAeqow7Gx5TsHxrYlzwJHHU_B",
+         "https://sun9-west.userapi.com/sun9-56/s/v1/if1/3aNTcY7iHbySv6CA4ezw5A2aYHLv89gnEC-DSYEqLK1tBMIfr5ohiAy9E-Fdxda3H25lwfLT.jpg?size=988x1080&quality=96&type=album"
       ],
+
       [
-        "ENA_WD & DackFax", "Новая жизнь", "https://drive.google.com/uc?export=download&id=1mBpZuE8sCnE-kj40oQZPOqxy-9sBo6-R",
-         "https://sun9-49.userapi.com/s/v1/if2/cSIH3xVQuG0RaRCI2dfBW_hCzZCfLxlmfhnuECjA41nDjWVOmpBt3GuDe2k_Cw3mYIqb99n_1_b4Owf4rd7ay8tE.jpg?size=1920x1080&quality=96&type=album"
-      ],
-      [
-        "ENA_WD & DackFax", "Просвистела", "https://drive.google.com/uc?export=download&id=1BB3ktiugoKhTIYPqEBClTWG89K1E-KCX",
-         "https://sun9-21.userapi.com/s/v1/if1/kfO-6fbGm6B78OSHeLmoeGSBS__kvG7l9-iYPJpT055O-T-mzAexwzGB7XflsQamnCOZgy20.jpg?size=960x1280&quality=96&type=album"
-      ],
-      [
-        "Дворовый стиль", "Вороны", "https://drive.google.com/uc?export=download&id=10mnAwtb-KBQitwEDheWh0zGj3y5yim44",
-         "https://sun9-12.userapi.com/s/v1/ig2/buaspJUYifMeZp1RTfPR9eI6_srfdiO-_thvTAqMQNqFlSUAnVEXI2qEcs4m2UzKIu9s_AfZJrBs3OPZQST3wIAQ.jpg?size=786x1080&quality=96&type=album"
-      ],
-      [
-        "ENA_WD & DackFax", "Далеко", "https://drive.google.com/uc?export=download&id=1r0zKdpe1-pbLZfKPCNzbpIVIw6Vcs315",
-         "https://sun9-80.userapi.com/s/v1/if1/KwVbU01CJFoYuemlfSjffv7S2YKh1v1ggTMvjywRRxveZetelMXw0KS8znCFOs_yXhGOigDB.jpg?size=1280x1024&quality=96&type=album"
-      ],
-      [
-        "Шпионка Мари", "Дождь", "https://drive.google.com/uc?export=download&id=1PYmsuxDz_QKzxPNy-lvQ9BuiWQCVVemT",
+        "Шпионка Мари", "Сердце", "https://drive.google.com/uc?export=download&id=1QU17af4I3OePI15t5iHiNmrIzsGBLOx5",
          "https://sun9-83.userapi.com/s/v1/if2/NZ68F_l7zkKvJ-jmeii3x1ySDxWAMLuVLEdTflq1Hx-yeEtd3vuz0Ei8ZPHJ88XDA4Gj4FQufDoaH4WftXD0ZKDe.jpg?size=900x1600&quality=96&type=album"
       ],
       [
-        "Медоманы", "Метель", "https://drive.google.com/uc?export=download&id=1scicA1CGXdxqdIQurSrt_tKi-58bkb8r",
-         "https://drive.google.com/uc?export=download&id=1y0BhTmvh9_JJeyTk75bu-TWhoyHlNPBk"
+        "Николай Зайцев", "Дочь Самурая", "https://drive.google.com/uc?export=download&id=1x7K2Uu-JVQuGzAzJ_oWYojcmCZY0S8U_",
+         "https://sun9-east.userapi.com/sun9-43/s/v1/if1/GwmwD98zYDjdBR55XFk9qmXZH6yiSpLY7E7vghBa738yPxmDQOVxgk9sGHcpAoQvCXTIkTM8.jpg?size=1620x2160&quality=96&type=album"
       ],
       [
-        "Василий Рыжкин", "Это всё", "https://drive.google.com/uc?export=download&id=1F8jB1Oq8zECd74pSXotAktJS4P3ZNGx_",
-         "https://sun9-17.userapi.com/s/v1/ig2/7bi3HGIsG3xPbdZJZ2IRmpB5UxaBzluDOW8Ucj3gOmoqb1Z2MZ8ElzBmYFaGTLYnF8DWYgTUxfmsHY-tH7j7446k.jpg?size=1600x1200&quality=95&type=album"
+        "Шпионка Мари", "Мороз по коже", "https://drive.google.com/uc?export=download&id=1gjjAIcFoHqGSabo-TfbKv1dpZC-4dVZc",
+         "https://sun9-83.userapi.com/s/v1/if2/NZ68F_l7zkKvJ-jmeii3x1ySDxWAMLuVLEdTflq1Hx-yeEtd3vuz0Ei8ZPHJ88XDA4Gj4FQufDoaH4WftXD0ZKDe.jpg?size=900x1600&quality=96&type=album"
       ],
       [
-        "Жанат Акбулатов", " Мёртвый город. Рождество", "https://drive.google.com/uc?export=download&id=1K-n_Br5unktNZG4xFrurgoRxgJ9sg1JZ",
-         "https://sun9-52.userapi.com/s/v1/if1/-HmDP5EHoOqGgOfoXilfVJ3FTVxKRWibkPVOPEwojlIPoEGCdm7fRqLRfQ97cVoT99vCSgKb.jpg?size=490x679&quality=96&type=album"
+        "Николай Зайцев", "Орбит без сахара", "https://drive.google.com/uc?export=download&id=1YvtHEmybr2Iq7zBiNppGxQEeM0wFGswE",
+         "https://sun9-east.userapi.com/sun9-43/s/v1/if1/GwmwD98zYDjdBR55XFk9qmXZH6yiSpLY7E7vghBa738yPxmDQOVxgk9sGHcpAoQvCXTIkTM8.jpg?size=1620x2160&quality=96&type=album"
+      ],
+      [
+        "Василий Рыжкин", "Линия жизни", "https://drive.google.com/uc?export=download&id=1fn0KOc5QPtb-9W9C0crC2fyGGTbJzEld",
+         "https://sun9-west.userapi.com/sun9-12/s/v1/ig2/WBJKA6PPcmUIzvxImLVQkg9sCdUefrEEDqhE306tULZGjzZn08TQ84-t4DPHWpkebnJlkuh6KGqRprRN6AuAOl5_.jpg?size=958x1280&quality=96&type=album"
+      ],
+      [
+        "Василий Рыжкин", "Новые люди", "https://drive.google.com/uc?export=download&id=1kqsUi67p-GwqIm-D3mOCmYkbgzCJjW4M",
+         "https://sun9-west.userapi.com/sun9-12/s/v1/ig2/WBJKA6PPcmUIzvxImLVQkg9sCdUefrEEDqhE306tULZGjzZn08TQ84-t4DPHWpkebnJlkuh6KGqRprRN6AuAOl5_.jpg?size=958x1280&quality=96&type=album"
+      ],
+      [
+        "Жанат Акбулатов", " Моя любовь", "https://drive.google.com/uc?export=download&id=1dfhTAia4renAHtQUPwuELGAkvKeq6zyG",
+         "https://sun9-east.userapi.com/sun9-59/s/v1/if2/E8Tp_8c2ohYFCLtAirJe1uc7k5BTFyrfPSoAIbRX3gTb4nwn2Qr8S9oxga2QdgXeHQc5gjTgiMTofqK1rr120_iy.jpg?size=400x400&quality=95&type=album"
+      ],
+      [
+        "Жанат Акбулатов", "Скажи", "https://drive.google.com/uc?export=download&id=1ducPiuhjzibvTfxErHMRWAWMU1npHUFZ",
+         "https://sun9-east.userapi.com/sun9-59/s/v1/if2/E8Tp_8c2ohYFCLtAirJe1uc7k5BTFyrfPSoAIbRX3gTb4nwn2Qr8S9oxga2QdgXeHQc5gjTgiMTofqK1rr120_iy.jpg?size=400x400&quality=95&type=album"
       ],
 
     ];
